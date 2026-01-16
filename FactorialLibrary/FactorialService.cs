77d7c97b574660646943f4d27c0f1ea9e5ca33a9
s-godbo <samantha.godbold@bsu.edu>
2026-01-16 18:42:56 +0000

    initial factorial project files

---
+++ FactorialLibrary/FactorialService.cs
@@ -0,0 +1,11 @@
+﻿namespace FactorialLibrary;
+
+public class FactorialService
+{
+    public static int Factorial(int n) {
+        if(n==0 || n==1) { //0!=1   1!=1
+            return 1;
+        }
+        return n*Factorial(n-1); //5! = 5*4!
+    }
+}
